5e9661f126db35ec6ceb1b01ba9b3a95f281cf7b
mammadli <gulnar.mammadli@ut.ee>
2023-03-18 15:17:10 +0200

    Server.py set_symbol changed

--- server.py
+++ server.py
@@ -1,4 +1,3 @@
-import uuid
 import grpc
 from concurrent import futures
 import game_pb2
@@ -7,109 +6,37 @@ import game_pb2_grpc
 
 
 class PlayerServiceServicer(game_pb2_grpc.PlayerServiceServicer):
-    def __init__(self) -> None:
+    def __init__(self,channel) -> None:
         super().__init__()
         self.board = [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']]
         self.player1_symbol = 'X'
         self.player2_symbol = 'O'
         self.current_player = self.player1_symbol
-        self.games = {}
-        self.players = {}
+        self.stub = game_pb2_grpc.AdminServiceStub(channel)
 
     def set_symbol(self, request, context):
-        if request.symbol not in [self.player1_symbol, self.player2_symbol]:
+        request = game_pb2.PlayerRequest()
+        request_data = self.stub.set_symbol(request)
+        if request_data.symbol not in [self.player1_symbol, self.player2_symbol]:
             context.set_details('Invalid symbol')
             context.set_code(grpc.StatusCode.INVALID_ARGUMENT)
             return game_pb2.SymbolResponse(success=False)
-        if self.current_player != request.symbol:
+        if self.current_player != request_data.symbol:
             context.set_details('Not your turn')
             context.set_code(grpc.StatusCode.FAILED_PRECONDITION)
             return game_pb2.SymbolResponse(success=False)
-        if self.board[request.row][request.col] != '-':
+        if self.board[request_data.row][request_data.col] != '-':
             context.set_details('Cell already occupied')
             context.set_code(grpc.StatusCode.FAILED_PRECONDITION)
             return game_pb2.SymbolResponse(success=False)
 
-        self.board[request.row][request.col] = request.symbol
+        self.board[request_data.row][request_data.col] = request_data.symbol
         if self.current_player == self.player1_symbol:
             self.current_player = self.player2_symbol
         else:
             self.current_player = self.player1_symbol
         return game_pb2.SymbolResponse(success=True)
 
-
-    # draft
-    def start_game(self, request, context):
-        player_id = request.player_id
-        if player_id in self.players:
-            return game_pb2.GameId(game_id=self.players[player_id])
-        else:
-            game_id = str(uuid.uuid4())
-            self.games[game_id] = {'board': [' '] * 9, 'next_player': 'X', 'winner': None}
-            self.players[player_id] = game_id
-            return game_pb2.GameId(game_id=game_id)
-
-    def join_game(self, request, context):
-        player_id = request.player_id
-        game_id = request.game_id
-        if game_id not in self.games:
-            context.set_code(grpc.StatusCode.NOT_FOUND)
-            context.set_details('Game not found')
-            return game_pb2.Empty()
-        elif self.games[game_id]['winner'] is not None:
-            context.set_code(grpc.StatusCode.FAILED_PRECONDITION)
-            context.set_details('Game is over')
-            return game_pb2.Empty()
-        elif len([p for p in self.players.values() if p == game_id]) == 2:
-            context.set_code(grpc.StatusCode.FAILED_PRECONDITION)
-            context.set_details('Game is full')
-            return game_pb2.Empty()
-        else:
-            self.players[player_id] = game_id
-            return game_pb2.Empty()
-
-    def make_move(self, request, context):
-        player_id = request.player_id
-        game_id = self.players.get(player_id)
-        if game_id is None:
-            context.set_code(grpc.StatusCode.FAILED_PRECONDITION)
-            context.set_details('Player not in a game')
-            return game_pb2.Empty()
-        game = self.games.get(game_id)
-        if game is None:
-            context.set_code(grpc.StatusCode.NOT_FOUND)
-            context.set_details('Game not found')
-            return game_pb2.Empty()
-        if game['winner'] is not None:
-            context.set_code(grpc.StatusCode.FAILED_PRECONDITION)
-            context.set_details('Game is over')
-            return game_pb2.Empty()
-        if player_id != game['next_player']:
-            context.set_code(grpc.StatusCode.FAILED_PRECONDITION)
-            context.set_details('Not player\'s turn')
-            return game_pb2.Empty()
-        pos = request.position
-        if pos < 0 or pos > 8:
-            context.set_code(grpc.StatusCode.INVALID_ARGUMENT)
-            context.set_details('Invalid position')
-            return game_pb2.Empty()
-        if game['board'][pos] != ' ':
-            context.set_code(grpc.StatusCode.FAILED_PRECONDITION)
-            context.set_details('Position already taken')
-            return game_pb2.Empty()
-        game['board'][pos] = game['next_player']
-        if self.check_for_win(game['board']):
-            game['winner'] = game['next_player']
-            return game_pb2.MoveResult(result=game_pb2.MoveResult.WIN)
-        elif self.check_for_draw(game['board']):
-            game['winner'] = 'DRAW'
-            return game_pb2.MoveResult(result=game_pb2.MoveResult.DRAW)
-        else:
-            game['next_player'] = 'O' if game['next_player'] == 'X' else 'X'
-            return game_pb2.MoveResult(result=game_pb2.MoveResult.CONTINUE)
-
     def list_board(self, request, context):
-        player_id = request.player_id
-        game_id = self.players.get(player_id)
-        if game_id is None:
-            context.set
+        board_str = '\n'.join([' '.join(row) for row in self.board])
+        return game_pb2.BoardResponse(board=board_str)
