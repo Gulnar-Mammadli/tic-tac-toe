8baee2bb56a34e53410336a028010f5424537e68
Gandabh <qendabhesenova@gmail.com>
2023-03-18 12:43:10 +0200

    server file changed and methdos added for first phase

--- server.py
+++ server.py
@@ -8,18 +8,32 @@ import game_pb2_grpc
 class PlayerServiceServicer(game_pb2_grpc.PlayerServiceServicer):
     def __init__(self) -> None:
         super().__init__()
+        self.board = [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']]
+        self.player1_symbol = 'X'
+        self.player2_symbol = 'O'
+        self.current_player = self.player1_symbol
 
     def set_symbol(self, request, context):
-        pass
-    
+        if request.symbol not in [self.player1_symbol, self.player2_symbol]:
+            context.set_details('Invalid symbol')
+            context.set_code(grpc.StatusCode.INVALID_ARGUMENT)
+            return game_pb2.SymbolResponse(success=False)
+        if self.current_player != request.symbol:
+            context.set_details('Not your turn')
+            context.set_code(grpc.StatusCode.FAILED_PRECONDITION)
+            return game_pb2.SymbolResponse(success=False)
+        if self.board[request.row][request.col] != '-':
+            context.set_details('Cell already occupied')
+            context.set_code(grpc.StatusCode.FAILED_PRECONDITION)
+            return game_pb2.SymbolResponse(success=False)
+
+        self.board[request.row][request.col] = request.symbol
+        if self.current_player == self.player1_symbol:
+            self.current_player = self.player2_symbol
+        else:
+            self.current_player = self.player1_symbol
+        return game_pb2.SymbolResponse(success=True)
+
     def list_board(self, request, context):
-        pass
-
-# Increase the maximum metadata size
-options = [
-    ('grpc.max_send_message_length', 50 * 1024 * 1024),  # 50 MB
-    ('grpc.max_receive_message_length', 50 * 1024 * 1024),  # 50 MB
-]
-
-def serve():
-    player_service_servicer = PlayerServiceServicer()
+        board_str = '\n'.join([' '.join(row) for row in self.board])
+        return game_pb2.BoardResponse(board=board_str)
