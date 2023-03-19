1c414da6adea5b1aee8fa0f52b17ac12ac4303dc
mammadli <gulnar.mammadli@ut.ee>
2023-03-19 12:00:13 +0200

    Add broadcastMessage and update set_symbol

--- client.py
+++ client.py
@@ -2,6 +2,7 @@ import grpc
 import random
 import game_pb2
 import game_pb2_grpc
+import datetime
 
 class Client():
 
@@ -12,6 +13,7 @@ class Client():
         self.channel= grpc.insecure_channel(f"{self.serverip}:{self.first_port}")
         self.stub1 = game_pb2_grpc.PlayerServiceStub(self.channel)
         self.stub2 = game_pb2_grpc.AdminServiceStub(self.channel)
+        self.reg_timestamp = str(datetime.datetime.now())
         self.total_processes = 3
     def access_to_server(self):
     
@@ -20,14 +22,25 @@ class Client():
             request = game_pb2.AccessRequest()
             request.name = self.reg_name
             response = self.stub1.access_to_server(request)
+            self.reg_symbol = response.symbol
             print(f"your id: {response.id} symbol {response.symbol}")  # do something with the response object
             return response
-    def start_game(self):
+    def set_symbol(self):
             request = game_pb2.PlayerRequest()
+            request.position = self.reg_position
+            request.symbol = self.reg_symbol
+            request.timestamp = self.reg_timestamp
             self.stub2.start_game(request)
+
+    def broadcastMessage(self):
+        msg = game_pb2.MessageRequest(message = "hi")
+        response_iterator = self.stub2.BroadcastMessage(msg)
+        for response in response_iterator:
+            print(response.message)
+
     # first_port = 50051
 
 if __name__ == "__main__": 
         a = Client()
         a.access_to_server()
-        a.start_game()    
+        a.set_symbol()    
